1a93f3baf594b04181887379922414f5f10355dd
MuhamadRyanHidayat <ryanhidayat074@gmail.com>
2020-02-12 11:14:40 +0700

    BIOSKOP

---
+++ resources/views/bioskop.blade.php
@@ -0,0 +1,730 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>CINEMA XXI</title>
+  </head>
+  <style>
+      #search{
+        position: absolute;
+width: 350px;
+height: 30px;
+left: 960px;
+top: 30px;
+border-radius: 10px;
+background: #FFFFFF;
+box-shadow: inset 0px 4px 4px rgba(111, 108, 108, 0.25);
+text-align: center;
+      }
+  </style>
+  <body>
+  <nav class="navbar navbar-light" id="nav-atas" style="background-color: #005C99;">
+  <a href="#"><img src="img/home.png" width="45px" alt="Home"></a><a href="#"><img src="img/cinema.png" alt="cinema" style="margin-right: 1055px;"></a>
+  <form class="form-inline">
+  <input type="text" name="search" class="search" id="search" placeholder="Search theater, Movie...">
+  </form>
+</nav>
+<nav class="navbar navbar-light bg-light">
+  <div class="row">
+    <div class="col">
+
+      <a href=""><img src="img/playbutton.png" width="20px" alt=""></a><a href="#" class="navbar-brand" style="margin-right: auto; margin-left: 5px;">Now Playing</a>
+      <a href=""><img src="img/upcoming1.png" width="20px" style="margin-left: 20px;" alt=""></a><a href="#" class="navbar-brand" style="margin-left: 5px; margin-right: 1000px;" >Up Coming</a>
+      <hr style="
+    position: absolute;
+    width: 50px;
+    height: 0px;
+    left: 700px;
+    top: 11px;
+    
+    border: 1px solid #000000;
+    transform: rotate(90deg);">
+    
+
+    <img src="img/imaxe17.png" alt="" style="width: 100px; height: 20px; margin-left: 750px;  margin-top: -50px;" >
+    <img src="img/doble18.png" alt="" style="width: 120px; height: 20px; margin-left: 880px;  margin-top: -90px;" >
+    
+
+
+  </nav>
+
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" id="slide">
+    <div class="carousel-inner" style="position: absolute;
+    width: 925px;
+    height: 527px;
+    left: 60px;
+    top: 20px;">
+      <div class="carousel-item active">
+        <img src="img/dilan.jpg" class="d-block w-100" alt="dilan">
+      </div>
+      <div class="carousel-item">
+        <img src="img/fantasyland.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/little woman.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/kajeng kliwon.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+   <img src="img/gopay.png" alt="gopay" class="gambarkanan" style="position: absolute;
+   width: 320px;
+   height: 185px;
+   left: 1000px;
+   top: 230px;">
+   <img src="img/diskon.png" alt="diskon" style="position: absolute;
+   width: 320px;
+   height: 185px;
+   left: 1000px;
+   top: 450px;">
+
+
+   <!-- OPTION -->
+
+    <hr style="
+    position: absolute;
+width: 50px;
+height: 0px;
+left: 60px;
+top: 800px;
+border: 1px solid #000000;
+transform: rotate(90.38deg);"> <p style="
+position: absolute;
+width: 199px;
+height: 35px;
+left: 86px;
+top: 800px;
+
+background-color: #005C99;
+text-align: center;
+
+font-family: Roboto Mono;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 35px;
+ 
+color: #FFFFFF;">Now Playing</p><select class="form-control form-control-sm" style="
+position: absolute;
+width: 232px;
+height: 36px;
+left: 285px;
+top: 800px;
+
+background: #C4C4C4;">
+                                  <option>JAKARTA</option>
+                                  <option>AMBON</option>
+                                  <option>BALIKPAPAN</option>
+                                  <option>BANDUNG</option>
+                                  <option>BANJARMASIN</option>
+                                  <option>BATAM</option>
+                                  <option>BEKASI</option>
+                                  <option>BENGKULU</option>
+                                  <option>BOGOR</option>
+                                  <option>CIKARANG</option>
+                                  <option>CILEGON</option>
+                                  <option>CIREBON</option>
+                                  <option>DENPASAR</option>
+                                  <option>DUMAI</option>
+                                  <option>GARUT</option>
+                                  <option>GORONTALO</option>
+                                  </select>
+
+    <!-- GAMBAR POSTER!!!! -->
+    <a href="#"><img src="img/judy.png" alt="judy" style="position: absolute;
+    width: 335px;
+    height: 525px;
+    left: 80px;
+    top: 950px;
+    "></a>
+    <a href="#"><img src="img/assent.png" alt="setan" style="position: absolute;
+    width: 336px;
+    height: 525px;
+    left: 500px;
+    top: 950px;"></a>
+    <a href=""><img src="img/setan.png" alt="setan1" style="position: absolute;
+    width: 336px;
+    height: 525px;
+    left: 920px;
+    top: 950px;
+    color: black;"></a>
+    <a href="#" style="position: absolute;
+    width: 200px;
+    height: 28px;
+    left: 147px;
+    top: 1500px;
+    
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: black;
+    ">BIRDS OF PREY</a>
+    <a href="#" style="position: absolute;
+    width: 169px;
+    height: 28px;
+    left: 598px;
+    top: 1500px;
+    
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: black;
+    ">THE ASSENTS</a>
+    <a href="#" style="position: absolute;
+    width: 169px;
+    height: 28px;
+    left: 1020px;
+    top: 1500px;
+    
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: black;
+    ">MANGKUJIWO</a>
+    <p style="position: absolute;
+    width: 80px;
+    height: 36px;
+    left: 205px;
+    top: 1550px;
+
+    background-color: red;
+    color: #FFFFFF;
+    border-radius: 2px;
+    width: 70px;
+    text-align: center;
+    
+    font-family: Oswald;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;">D 17+</p>
+    <p style="position: absolute;
+    width: 80px;
+    height: 36px;
+    left: 650px;
+    top: 1550px;
+
+    background-color: red;
+    color: #FFFFFF;
+    border-radius: 2px;
+    width: 70px;
+    text-align: center;
+    
+    font-family: Oswald;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;">D 17+</p>
+    <p style="position: absolute;
+    width: 80px;
+    height: 36px;
+    left: 1080px;
+    top: 1550px;
+
+    background-color: red;
+    color: #FFFFFF;
+    border-radius: 2px;
+    width: 70px;
+    text-align: center;
+    
+    font-family: Oswald;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;">D 17+</p>
+
+    <!-- GAMBAR POSTER BARIS 2 -->
+    <a href="#"><img src="img/dolittle.png" alt="stark" style="position: absolute;
+      width: 335px;
+      height: 525px;
+      left: 80px;
+      top: 1650px;
+      "></a>
+      <a href="#"><img src="img/badboy.jpg" alt="playboy" style="position: absolute;
+        width: 336px;
+        height: 525px;
+        left: 500px;
+        top: 1650px;"></a>
+      <a href=""><img src="img/kisahdoel.jpg" alt="setan1" style="position: absolute;
+        width: 336px;
+        height: 525px;
+        left: 920px;
+        top: 1650px;"></a>
+
+<a href="#" style="position: absolute;
+width: 200px;
+height: 28px;
+left: 147px;
+top: 2200px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color: black;
+">DOLITTLE</a>
+
+<a href="#" style="position: absolute;
+    width: 169px;
+    height: 28px;
+    left: 598px;
+    top: 2200px;
+    text-align: center;
+    
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: black;
+    ">BAD BOYS</a>
+
+<a href="#" style="position: absolute;
+width: 200px;
+height: 28px;
+left: 1020px;
+top: 2200px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color: black;
+">Akhir Cinta SiDoel</a>
+
+<p style="position: absolute;
+    width: 80px;
+    height: 36px;
+    left: 205px;
+    top: 2250px;
+
+    background-color: green;
+    color: #FFFFFF;
+    border-radius: 2px;
+    width: 70px;
+    text-align: center;
+    
+    font-family: Oswald;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;">SU</p>
+
+<p style="position: absolute;
+width: 80px;
+height: 36px;
+left: 650px;
+top: 2250px;
+
+background-color: red;
+color: #FFFFFF;
+border-radius: 2px;
+width: 70px;
+text-align: center;
+
+font-family: Oswald;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 36px;">D 17+</p>
+
+<p style="position: absolute;
+    width: 80px;
+    height: 36px;
+    left: 1080px;
+    top: 2250px;
+
+    background-color: blue;
+    color: #FFFFFF;
+    border-radius: 2px;
+    width: 70px;
+    text-align: center;
+    
+    font-family: Oswald;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;">R 13+</p>
+
+     <!-- PENUTUP!!! -->
+
+<hr style="
+    position: absolute;
+width: 50px;
+height: 0px;
+left: 60px;
+top: 2400px;
+border: 1px solid #000000;
+transform: rotate(90.38deg);"><p style="
+position: absolute;
+width: 199px;
+height: 35px;
+left: 86px;
+top: 2400px;
+
+background-color: #005C99;
+text-align: center;
+
+font-family: Roboto Mono;
+font-style: normal;
+font-weight: bold;
+font-size: 25px;
+line-height: 35px;
+ 
+color: #FFFFFF;">Up Coming</p>
+
+ <!-- PARAGRAF 2 -->
+
+ <a href="#"><img src="img/underwater.jpg" alt="water" style="position: absolute;
+  width: 335px;
+  height: 525px;
+  left: 80px;
+  top: 2500px;
+  "></a>
+  <a href="#"><img src="img/ipman.jpg" alt="ipman" style="position: absolute;
+  width: 336px;
+  height: 525px;
+  left: 500px;
+  top: 2500px;"></a>
+  <a href=""><img src="img/poison.jpg" alt="posion" style="position: absolute;
+  width: 336px;
+  height: 525px;
+  left: 920px;
+  top: 2500px;
+  color: black;"></a>
+  <a href="#" style="position: absolute;
+  width: 200px;
+  height: 28px;
+  left: 147px;
+  top: 3050px;
+  
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: black;
+  ">UNDERWATER</a>
+  <a href="#" style="position: absolute;
+  width: 169px;
+  height: 28px;
+  left: 598px;
+  top: 3050px;
+  
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: black;
+  ">IP MAN 4</a>
+  <a href="#" style="position: absolute;
+  width: 300px;
+  height: 28px;
+  left: 995px;
+  top: 3050px;
+  
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: black;
+  ">THE POSION ROSE</a>
+
+ <p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 205px;
+  top: 3100px;
+
+  background-color: blue;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 70px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">R 13+</p>
+  <p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 609px;
+  top: 3100px;
+
+  background-color: red;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 70px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">D 17+</p>
+  <p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 1070px;
+  top: 3100px;
+
+  background-color: red;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 70px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">D 17+</p>
+
+
+   <!-- GAMBAR POSTER BARIS 2 -->
+  <a href="#"><img src="img/semesta.jpg" alt="semesta" style="position: absolute;
+    width: 335px;
+    height: 525px;
+    left: 80px;
+    top: 3200px;
+    "></a>
+    <a href="#"><img src="img/bomb.jpg" alt="bomb" style="position: absolute;
+      width: 336px;
+      height: 525px;
+      left: 500px;
+      top: 3200px;"></a>
+    <a href=""><img src="img/nikahyu.jpg" alt="nikah" style="position: absolute;
+      width: 336px;
+      height: 525px;
+      left: 920px;
+      top: 3200px;"></a>
+
+<a href="#" style="position: absolute;
+width: 200px;
+height: 28px;
+left: 147px;
+top: 3750px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color: black;
+">SEMESTA</a>
+
+<a href="#" style="position: absolute;
+  width: 169px;
+  height: 28px;
+  left: 598px;
+  top: 3750px;
+  text-align: center;
+  
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: black;
+  ">MY SPY</a>
+
+<a href="#" style="position: absolute;
+width: 200px;
+height: 28px;
+left: 1020px;
+top: 3750px;
+text-align: center;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color: black;
+">NIKAH YUK!</a>
+
+<p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 205px;
+  top: 3800px;
+
+  background-color: green;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 70px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">SU</p>
+
+<p style="position: absolute;
+width: 80px;
+height: 36px;
+left: 650px;
+top: 3800px;
+
+background-color: blue;
+color: #FFFFFF;
+border-radius: 2px;
+width: 70px;
+text-align: center;
+
+font-family: Oswald;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 36px;">R 13+</p>
+
+<p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 1080px;
+  top: 3800px;
+
+  background-color: blue;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 70px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">R 13+</p> 
+
+  <a href="#"><p style="position: absolute;
+  width: 80px;
+  height: 36px;
+  left: 620px;
+  top: 3950px;
+
+  background-color: #005C99;
+  color: #FFFFFF;
+  border-radius: 2px;
+  width: 150px;
+  text-align: center;
+  
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;">View More</p></a>
+ 
+  <!-- FOOTER! -->
+
+  <footer style="position: absolute;
+  width: 1349px;
+  height: 316px;
+  left: 0px;
+  top: 4100px;
+  
+  background: #005C99;">
+  <a href="#"><p style="position: absolute;
+  width: 77px;
+  height: 31px;
+  left: 50px;
+  top: 100px;
+   color: #FFFFFF;
+  
+  font-family: Lora;
+  font-style: normal;
+  font-weight: regular;
+  font-size: 24px;
+  line-height: 31px;">Profile</p></a> <hr style="
+   position: absolute;
+width: 50px;
+height: 0px;
+left: 100px;
+top: 100px;
+border: 1px solid #ffffff;
+transform: rotate(90.38deg);">
+ <a href="#"><p style="position: absolute;
+  width: 150px;
+  height: 31px;
+  left: 140px;
+  top: 100px;
+   color: #FFFFFF;
+  
+  font-family: Lora;
+  font-style: normal;
+  font-weight: regular;
+  font-size: 24px;
+  line-height: 31px;">Terms Of Use</p></a>
+   <hr style="
+   position: absolute;
+width: 50px;
+height: 0px;
+left: 280px;
+top: 100px;
+border: 1px solid #ffffff;
+transform: rotate(90.38deg);">
+<a href="#"><p style="position: absolute;
+width: 150px;
+height: 31px;
+left: 330px;
+top: 100px;
+ color: #FFFFFF;
+
+font-family: Lora;
+font-style: normal;
+font-weight: regular;
+font-size: 24px;
+line-height: 31px;">Advertising</p></a>
+<p style="position: absolute;
+width: 1000pxpx;
+height: 31px;
+left: 50px;
+top: 200px;
+ color: #FFFFFF;
+
+font-family: Lora;
+font-style: normal;
+font-weight: regular;
+font-size: 14px;
+line-height: 31px;">© 1999-2012 21Cineplex.com. All materials and contents (texts, graphics, and every attributes) of 21Cineplex or 21Cineplex.com website are copyrights and trademarks of PT Nusantara Sejahtera Raya.
+Any commercial usage of the materials and contents is forbidden without prior permission from PT Nusantara Sejahtera Raya. There is no other institutions/agencies outside
+PT Nusantara Sejahtera Raya allowed to use www.21Cineplex.com (21Cineplex website) without prior permission from PT Nusantara Sejahtera Raya</p>
+   
+</footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
